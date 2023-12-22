234509cf4cdaea256fa7df0036c62ac2af7130d1
sharifrabbyny <139313695+sharifrabbyny@users.noreply.github.com>
2023-12-21 20:25:19 -0500

    Add files via upload

---
+++ calculateExtends.java
@@ -0,0 +1,23 @@
+package calculate;
+
+public class calculateExtends extends Calculate{
+	
+	private double blablabla;
+	
+	
+	public calculateExtends() {
+		
+		blablabla = 1000;
+	}
+
+
+	public double getBlablabla() {
+		return blablabla;
+	}
+
+
+	public void setBlablabla(double blablabla) {
+		this.blablabla = blablabla;
+	}
+
+}
